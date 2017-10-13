0bc225b42345c5f8a45483436446fc36d4c5e7df
WilsonkwSheng <wilsonkhoo93@gmail.com>
2017-10-13 18:29:22 +0800

    user can signup, login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
 	def index
- 		@users = User.all
  	end
  
  	def new
@@ -10,17 +9,21 @@ class UsersController < ApplicationController
  	def create
  		@user = User.new(user_params)
  		if @user.save
- 			redirect_to root_path
+ 			redirect_to user_path(@user)
  			flash[:success] = "Created the User!"
  		else
- 			render "index"
+ 			render "new"
  			flash[:notice] = "Didnt work"
  		end
+ 	end
+
+ 	def show
+ 		@user = User.find(params[:id])
  	end
  
  	private
  
  	def user_params
-     params.require(:user).permit(:email, :password)
-   end
+    params.require(:user).permit(:name, :email, :password, :address, :city, :state, :zip, :phone_number)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  resources :users
+	root 'welcome#index'
+
+	get '/sign_up', to: 'users#new'
+	post '/users', to: 'users#create'
+
+	get '/sign_in', to: 'sessions#new'
+	post '/sign_in', to: 'sessions#create'
+	get '/sign_out', to: 'sessions#destroy'
 end
