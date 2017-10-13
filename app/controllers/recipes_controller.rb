ff2298b0aec1aae2438a93b8cd8aca8f17023cf0
WilsonkwSheng <wilsonkhoo93@gmail.com>
2017-10-13 20:29:02 +0800

    able to create recipe

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,22 @@
+class RecipesController < ApplicationController
+	def new
+		@recipe = Recipe.new
+	end
+
+	def create
+		@recipe = Recipe.new(recipe_params)
+	if @recipe.save
+		redirect_to user_path(current_user)
+		flash[:success] = "Successfully create recipe listing"
+	else
+		render "new"
+		flash[:notice] = "Fail to create recipe listing"
+	end
+	end
+
+	private
+
+	def recipe_params
+		params.require(:recipe).permit(:title, :description, :price, :user_id)
+	end
+end
