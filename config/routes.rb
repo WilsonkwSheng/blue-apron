dae83c5841df2f6137496aa1dc126917d2f5ba48
WilsonkwSheng <wilsonkhoo93@gmail.com>
2017-10-13 18:50:06 +0800

    user able to login with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
 	get '/sign_in', to: 'sessions#new'
 	post '/sign_in', to: 'sessions#create'
 	get '/sign_out', to: 'sessions#destroy'
+
+	get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 end
