787ad52c44d9f1b5432e605a4e5231ea7d7081db
WilsonkwSheng <wilsonkhoo93@gmail.com>
2017-10-14 16:07:47 +0800

    add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 	has_secure_password
 	has_many :authentications, dependent: :destroy
 	has_many :recipes, dependent: :destroy
+
+	enum role: { :customer => 0, :admin => 1, :superadmin => 2 }
  
  	def self.create_with_auth_and_hash(authentication, auth_hash)
 		user = self.create!(
