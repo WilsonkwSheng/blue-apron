20b4e6ac3502824cfeb2ec849eb5b3363711d607
WilsonkwSheng <wilsonkhoo93@gmail.com>
2017-10-17 18:03:02 +0800

    pass capybara test

---
+++ spec/features/signin_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "User signin and signout", type: :feature do
+	describe "the signin process" do
+		before :each do
+			@user = User.create(name: "test", email: "example1@gmail.com", password: "123", address: "sungai mati", city: "kl", state: "kl", zip: "47400", phone_number: "999")
+		end
+
+		it "signs me in" do
+			visit sign_in_path
+	    fill_in 'email', with: 'example1@gmail.com'
+	    fill_in 'password', with: '123'
+	    click_button 'Sign in'
+	    expect(page).to have_content 'Signed in!'
+		end
+	end	
+end
